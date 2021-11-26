c9b2f808638944029abe12d0fe6f6f28f699c7d5
xx11KK11xx <94536940+xx11KK11xx@users.noreply.github.com>
2021-11-26 15:45:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Family Book","Nishith Kumar""Simant Deep", "Neha Mayanka", "Shrinika Kashyap", "Kushal Kashyap"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_name = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = [images]
+    "https://i.postimg.cc/MGn9GJXw/family.jpg",
+     "https://i.postimg.cc/wjMnFtMX/father.jpg" ,
+     "https://i.postimg.cc/bw5W5zSK/mother.jpg",
+      "https://i.postimg.cc/5ymDKL83/bro.jpg",
+       "https://i.postimg.cc/JnL6wtrd/sister.jpg",
+        
+    ;
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = [names] 
+    "Family",
+    "Simant",
+    "Neha",
+    "Shrinika",
+    "Kushal";
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
